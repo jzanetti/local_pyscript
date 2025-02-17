c90046740401cf64d4987b99e7cf6ed9db7c6f63
Sijin <zsjzyhzp@gmail.com>
2025-02-18 07:54:59 +1300

    update dowhy

--- examples/casual_learn_without_internet/main_dowhy.py
+++ examples/casual_learn_without_internet/main_dowhy.py
@@ -2,3 +2,17 @@ import numpy as np
 
 from dowhy import CausalModel
 import dowhy.datasets
+
+from pyscript import display
+
+data = dowhy.datasets.linear_dataset(beta=10,
+        num_common_causes=5,
+        num_instruments = 2,
+        num_effect_modifiers=1,
+        num_samples=5000,
+        treatment_is_binary=True,
+        stddev_treatment_noise=10,
+        num_discrete_common_causes=1)
+df = data["df"]
+
+display(df)
